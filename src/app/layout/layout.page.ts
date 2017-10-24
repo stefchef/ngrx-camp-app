f187facadec905dec1957ef9d71976a89d64ac62
stvo <stefan.voegeli@zuehlke.com>
2017-10-24 14:12:13 +0200

    add basket count badge,

--- src/app/layout/layout.page.ts
+++ src/app/layout/layout.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { RootState } from '../app.state';
 
 @Component({
   selector: 'app-layout',
@@ -7,8 +9,13 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./layout.page.scss']
 })
 export class LayoutPage implements OnInit {
+  basketItemCount: number;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private store: Store<RootState>) {
+    this.store.subscribe(s => {
+      this.basketItemCount = s.basketState.items.length;
+    });
+  }
 
   ngOnInit() {
   }
