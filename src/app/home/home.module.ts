2eb8c7d459af91f6420fae5116b404631dd28458
Baechi <Beat.Baechi@zuehlke.com>
2017-10-24 16:06:54 +0200

    Added new component for category.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '../layout/layout.module';
 import { PizzasComponent } from './pizzas/pizzas.component';
 import { PizzaComponent } from './pizzas/pizza/pizza.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
   ],
-  declarations: [PizzasComponent, PizzaComponent],
+  declarations: [PizzasComponent, PizzaComponent, CategoryComponent],
   exports : [PizzasComponent, PizzaComponent]
 })
 export class HomeModule { }
