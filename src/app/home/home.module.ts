b6c596bdc1e5b57d2b38f202645f7bd54a367064
Baechi <Beat.Baechi@zuehlke.com>
2017-10-24 16:29:32 +0200

    Fixed naming of categories.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutModule } from '../layout/layout.module';
 import { PizzasComponent } from './pizzas/pizzas.component';
 import { PizzaComponent } from './pizzas/pizza/pizza.component';
-import { CategoryComponent } from './category/category.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutModule,
   ],
-  declarations: [PizzasComponent, PizzaComponent, CategoryComponent],
+  declarations: [PizzasComponent, PizzaComponent, CategoriesComponent],
   exports : [PizzasComponent, PizzaComponent]
 })
 export class HomeModule { }
