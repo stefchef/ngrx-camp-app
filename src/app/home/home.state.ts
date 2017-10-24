353b0c362e2a073557dff3f1a3eaa14c23344c0f
Baechi <Beat.Baechi@zuehlke.com>
2017-10-24 16:10:44 +0200

    Moved category state to the category component.

--- src/app/home/home.state.ts
+++ src/app/home/home.state.ts
@@ -1,14 +1,12 @@
 import { Pizza } from './pizzas/pizza';
+import { Category } from './category';
 
 export interface HomeState {
     pizzas: Pizza[];
+    categories: Category[];
 }
 
 export const initialHomeState: HomeState = {
     pizzas: [],
+    categories: [],
 };
-
-export interface Category {
-    name: string;
-    id: string;
-}
