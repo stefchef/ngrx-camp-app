0d5b49a5a7626da7875a2e422b910212358da864
Baechi <Beat.Baechi@zuehlke.com>
2017-10-25 14:05:49 +0200

    Implemented get of options of the selected toppings.

--- src/app/home/custompizza/custompizza.state.ts
+++ src/app/home/custompizza/custompizza.state.ts
@@ -2,11 +2,6 @@ export interface CustomPizzaState {
     toppingOptions: Topping[];
 }
 
-export interface CustomPizza {
-    id: string;
-    toppings: Topping[];
-}
-
 export interface Topping {
     name: string;
     price: number;
