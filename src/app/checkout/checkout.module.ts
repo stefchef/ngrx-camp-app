7374bb5779e5af26b50dbb701e4a2514d0d63161
stefchef <svwork@outlook.com>
2017-10-26 17:49:58 +0200

    fix

--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutPage } from './checkout.page';
+import { PizzaMaterialModule } from '../pizza-material/pizza-material.module';
+import { DeliverySettingsComponent } from '../basket/delivery-settings/delivery-settings.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PizzaMaterialModule,
   ],
   declarations: [CheckoutPage],
-  exports:[CheckoutPage]
+  exports: [CheckoutPage]
 })
 export class CheckoutModule { }
