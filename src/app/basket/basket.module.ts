31806042e106583d3ff41129eaf152b430bb19cf
Baechi <Beat.Baechi@zuehlke.com>
2017-10-26 16:16:02 +0200

    Added new delivery options component

--- src/app/basket/basket.module.ts
+++ src/app/basket/basket.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PizzaMaterialModule } from '../pizza-material/pizza-material.module';
+import { DeliverySettingsComponent } from './delivery-settings/delivery-settings.component';
 
 @NgModule({
   imports: [
     CommonModule,
     PizzaMaterialModule,
   ],
-  declarations: []
+  declarations: [DeliverySettingsComponent]
 })
 export class BasketModule { }
---
+++ src/app/basket/delivery-settings/delivery-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-settings',
+  templateUrl: './delivery-settings.component.html',
+  styleUrls: ['./delivery-settings.component.scss']
+})
+export class DeliverySettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
