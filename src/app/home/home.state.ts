1cf71eff5ddb221d16cfb741c7208bcd9a6bbe5c
Baechi <Beat.Baechi@zuehlke.com>
2017-10-26 13:02:47 +0200

    Resolved conflict

--- src/app/home/home.state.ts
+++ src/app/home/home.state.ts
@@ -5,15 +5,5 @@ export interface HomeState {
 }
 
 export const initialHomeState: HomeState = {
-<<<<<<< HEAD
-    pizzas: [],
-    categories: [],
-    customPizzaState: {
-        toppingOptions: [],
-        pizza: null,
-    },
-    selectedCategoryId: '1',
-=======
     test: ''
->>>>>>> 2a69fb88e14404691b1f1da00845b5c260e5f2b9
 };
