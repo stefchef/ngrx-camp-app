60b3e357a88cdc5610eb7fbcacd1604f8993869c
stefchef <svwork@outlook.com>
2017-10-27 13:57:54 +0200

    deselect on add to basket

--- src/app/custom-pizza/custom-pizza.page.ts
+++ src/app/custom-pizza/custom-pizza.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CustomPizzaState, Topping } from './custom-pizza.state';
-import { MatListOption, MatSnackBar } from '@angular/material';
+import { MatListOption, MatSnackBar, MatSelectionList } from '@angular/material';
 import { RootState } from '../app.state';
 import { Pizza } from '../pizzas/pizza/index';
 import { AddToBasket } from '../checkout/basket/index';
@@ -12,25 +12,26 @@ import { AddToBasket } from '../checkout/basket/index';
   styleUrls: ['./custom-pizza.page.scss']
 })
 export class CustomPizzaPage implements OnInit {
-
+  @ViewChild('toppings') private toppings: MatSelectionList;
   custompizzaState: CustomPizzaState;
   pizza: Pizza;
-  snackBarRef: MatSnackBar;
-
   constructor(private store: Store<RootState>, private snackBar: MatSnackBar) {
     this.store.subscribe(s => this.custompizzaState = s.customPizzaState);
     this.store.subscribe(s => this.pizza = s.pizzasState.pizzas.find(p => p.categoryId === '2'));
-    this.snackBarRef = snackBar;
   }
 
-  addCustomPizzaToCart(options: MatListOption[]) {
-    const toppings = <Topping[]>options.map(m => m.value);
+  addCustomPizzaToCart() {
+    const toppings = <Topping[]>this.toppings.selectedOptions.selected.map(m => m.value);
     const pizza = { ...this.custompizzaState.pizza };
     pizza.toppings = toppings;
-    this.snackBarRef.open('One custom pizza added to the basket.', null, { duration: 1000, });
+    this.snackBar.open('One custom pizza added to the basket.', null, { duration: 1000, });
     this.store.dispatch(new AddToBasket(pizza));
+    this.toppings.options.forEach(a => this.deselectNonDefault(a));
   }
-
+  deselectNonDefault(item: MatListOption) {
+    if ((<Topping>item.value).isDefault === false) { item.selected = false; return; }
+    item.selected = true;
+  }
   ngOnInit() {
   }
 
