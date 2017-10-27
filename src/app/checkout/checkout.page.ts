221139f75ca977994c05de5aaa7894a68e9af7aa
Baechi <Beat.Baechi@zuehlke.com>
2017-10-27 09:44:58 +0200

    Added styling for the delivery settings and the checkout.

--- src/app/checkout/checkout.page.ts
+++ src/app/checkout/checkout.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.page.scss']
 })
 export class CheckoutPage implements OnInit {
+  @HostBinding('class.page') true;
 
   constructor() { }
 
